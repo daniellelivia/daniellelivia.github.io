1c95b6bab3d696750441be81c86ca978507a8647
daniellelivia <danielle.livia.clarke@gmail.com>
2020-03-01 02:12:46 +1100

    Initial dist build

---
+++ precache-manifest.e22b0e78ccefb4db72a30c324e81db40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9690eab1820f7de3bf58",
+    "url": "/css/app.05b0fd4b.css"
+  },
+  {
+    "revision": "acc3df44e9fdb5fc52c2",
+    "url": "/css/chunk-vendors.0d0a218f.css"
+  },
+  {
+    "revision": "7862f47a6ff002f74fef458f67483ebf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9690eab1820f7de3bf58",
+    "url": "/js/app.ba615102.js"
+  },
+  {
+    "revision": "acc3df44e9fdb5fc52c2",
+    "url": "/js/chunk-vendors.973d7c69.js"
+  },
+  {
+    "revision": "f47b8cbd44cea0a266d4dd53c15be421",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
